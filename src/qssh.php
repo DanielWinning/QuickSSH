7c989276a7e2a526cf05e04c4214748a5681e65f
Danny <daniel@winningwebdesign.co.uk>
2022-01-09 11:20:10 +0000

    more consistency in messages

--- src/qssh.php
+++ src/qssh.php
@@ -11,10 +11,8 @@ $qssh = new App();
 $console = new Ansi(new StreamWriter(STDOUT));
 
 if (count($argv) < 2) {
-    $console->color([SGR::COLOR_FG_PURPLE])->bold()->text("--- QuickSSH ---")->reset()->lf();
-    $console->color()->italic()->text("The simple command line SSH credential and connection manager.")->reset()->lf()->lf();
-    $console->color([SGR::COLOR_FG_RED])->bold()->text("Usage:")->reset()->lf();
-    $console->color([SGR::COLOR_FG_WHITE_BRIGHT, SGR::COLOR_BG_PURPLE_BRIGHT])->text("qssh <command> [<args>]")->reset()->lf()->lf();
+    $console->color([SGR::COLOR_FG_PURPLE])->bold()->text("QuickSSH")->reset()->lf();
+    $qssh->printHelp($console);
 } else {
     $qssh->runCommand($argv[1], $argv);
 }
